a1b4a59f9ef4295999502f912254222cd063d8af
i0xi <lx@i0xi.cn>
2025-03-25 18:38:57 +0800

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,2 +1,2 @@
-node --experimental-default-type=module noname-server.js --server &
+node --experimental-default-type=commonjs noname-server.js --server &
 (cd game && node --experimental-default-type=commonjs server.js)
